7f97089f924aa70d0906ca6183e9fc031d63b3bd
Rajat <rajatprajapati09@gmail.com>
2023-05-05 02:14:05 +0530

    [rajat]Employeewage.js file checking attendance of employee

---
+++ EmployeeWage.js
@@ -0,0 +1,14 @@
+const prsent =1;
+
+let atendence =Math.floor(Math.random*10)%2;
+
+if (prsent==atendance)
+{
+  console.log('Employee is prsent');
+
+}
+else
+{
+    console.log('Employee is absent');
+
+}
