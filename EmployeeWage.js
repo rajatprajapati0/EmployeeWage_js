1463f288017e95bf02f946f0614ff7e20de84d29
Rajat <rajatprajapati09@gmail.com>
2023-05-05 03:12:49 +0530

    [rajat]calculating emp wage for 20 working day and max 160working hour  using While Loop

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,13 @@
 const fullday =1;
 const Halfday =2;
-
+const maxWorkingDay=20;
+const maxWorkingHour=160;
 const fullTimeHour=8;
 const partTimeHour=4;
 const wagePerHour=20;
 let empHour=0;
+let totalDay=0;
+
 function getWorkingHour()
 {
     switch(atendance)
@@ -21,15 +24,14 @@ function getWorkingHour()
     }
 
 }
-let count=0;
-for(let day =0;day<20;day++)
+while(totalDay <maxWorkingDay&& empHour <=maxWorkingHour)
 {
     atendance =Math.floor(Math.random()*10)%3;
     empHour+=getWorkingHour();
-count++;
+    totalDay++;
 }
 var employeeMonthlyWage=empHour*wagePerHour;
 
 
-console.log("emp Monthly Wage :$"+employeeMonthlyWage+" Total hour :"+empHour+" total day :"+count);
+console.log("emp Monthly Wage :$"+employeeMonthlyWage+" Total hour :"+empHour+" total day :"+totalDay);
 
